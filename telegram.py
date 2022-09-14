25d69c8cbcea8f0f7152a13a87b950b74ba06fba
blackiten <99394714+blackiten@users.noreply.github.com>
2022-09-14 22:55:01 +0300

    added some forum avatars

--- telegram.py
+++ telegram.py
@@ -17,10 +17,11 @@ def welcome(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("Подобрать ник~")
     item2 = types.KeyboardButton("Статус для ВК^^")
+    item3 = types.KeyboardButton("*Аватарки для форумов*")
 
-    markup.add(item1, item2)
+    markup.add(item1, item2, item3)
 
-    bot.send_message(message.chat.id, "Приветик, <b>{0.first_name}!</b> \nЯ бот, посвященный нулевым =)\nЯ могу сгенерировать тебе случайный никнейм или статус~~".format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
+    bot.send_message(message.chat.id, "Приветик, <b>{0.first_name}</b>! \nЯ бот, посвященный нулевым =)\nЯ могу сгенерировать тебе случайный никнейм или статус~~".format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
@@ -28,6 +29,15 @@ def get_text_messages(message):
         if message.text == 'Подобрать ник~':
             nickname = random.choice(config.nicknames)
             bot.send_message(message.chat.id, nickname)
+        elif message.text == '*Аватарки для форумов*':
+            markup = types.InlineKeyboardMarkup(row_width=1)
+            item1 = types.InlineKeyboardButton("Аниме \(^_^)/", callback_data='anime_pic')
+            item2 = types.InlineKeyboardButton("Эмо..(", callback_data='emo_pic')
+            item3 = types.InlineKeyboardButton("Dля Dево4еk***", callback_data='girly_pic')
+
+            markup.add(item1, item2, item3)
+
+            bot.send_message(message.chat.id, "*~Выбери тип аватарки~*", reply_markup=markup)
         # elif message.text == 'Цитаты для тебя<3':
         #
         #     markup = types.InlineKeyboardMarkup(row_width=1)
@@ -46,6 +56,23 @@ def get_text_messages(message):
             bot.send_message(message.chat.id, 'Я могу сгенерировать тебе случайный ник или статус, для этого нажми на кнопки внизу (^-^)')
         else:
             bot.send_message(message.chat.id, "Я тебя не понимаю =( Напиши /help")
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback_inline(call):
+     try:
+         if call.message:
+             if call.data == 'anime_pic':
+                 anime_pic = open(random.choice(config.anime_pics), 'rb')
+                 bot.send_animation(call.message.chat.id, anime_pic)
+             elif call.data == 'emo_pic':
+                 emo_pic = open(random.choice(config.emo_pics), 'rb')
+                 bot.send_animation(call.message.chat.id, emo_pic)
+             elif call.data == 'girly_pic':
+                 girly_pic = open(random.choice(config.girly_pics), 'rb')
+                 bot.send_animation(call.message.chat.id, girly_pic)
+
+     except Exception as e:
+         print(repr(e))
 
 # @bot.callback_query_handler(func=lambda call:True)
 # def callback_inline(call):
