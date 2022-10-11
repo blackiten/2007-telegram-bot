742c55bf1df57cdc61e15a118d662f1a91e7f14b
blackiten <breakeven2208@gmail.com>
2022-10-11 20:54:08 +0300

    optimised + logging

--- telegram.py
+++ telegram.py
@@ -3,16 +3,22 @@
 import telebot
 import random
 import config
+import sys
+import traceback
+import datetime
+import logging
 
 from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
+logger = telebot.logger
+telebot.logger.setLevel(logging.DEBUG)
 
 @bot.message_handler(commands=["start"])
 def welcome(message):
     sti = open('sticker/AnimatedSticker.tgs', 'rb')
     bot.send_sticker(message.chat.id, sti)
-
+    
     #keyboard
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("Подобрать ник~")
@@ -22,14 +28,14 @@ def welcome(message):
     markup.add(item1, item2, item3)
 
     bot.send_message(message.chat.id, "Приветик, <b>{0.first_name}</b>! \nЯ бот, посвященный нулевым =)\nЯ могу сгенерировать тебе случайный никнейм, статус или аву~~".format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
-
+    
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.chat.type == 'private':
-        if message.text == 'Подобрать ник~':
+        if message.text == 'Подобрать ник~' or message.text == '/nickname':
             nickname = random.choice(config.nicknames)
             bot.send_message(message.chat.id, nickname)
-        elif message.text == '*Аватарки для форумов*':
+        elif message.text == '*Аватарки для форумов*' or message.text == '/avatar':
             markup = types.InlineKeyboardMarkup(row_width=1)
             item1 = types.InlineKeyboardButton("Аниме \(^_^)/", callback_data='anime_pic')
             item2 = types.InlineKeyboardButton("Эмо..(", callback_data='emo_pic')
@@ -38,22 +44,11 @@ def get_text_messages(message):
             markup.add(item1, item2, item3)
 
             bot.send_message(message.chat.id, "*~Выбери тип аватарки~*", reply_markup=markup)
-        # elif message.text == 'Цитаты для тебя<3':
-        #
-        #     markup = types.InlineKeyboardMarkup(row_width=1)
-        #     item1 = types.InlineKeyboardButton("Веселое))", callback_data='good')
-        #     item2 = types.InlineKeyboardButton("Грустное((((", callback_data='bad')
-        #     item3 = types.InlineKeyboardButton("Дерзкое;)))", callback_data='cheeky')
-        #     item4 = types.InlineKeyboardButton("Задумчивое....", callback_data='wise')
-        #
-        #     markup.add(item1, item2, item3, item4)
-        #
-        #     bot.send_message(message.chat.id, "Какое у тебя настроение??", reply_markup=markup)
-        elif message.text == 'Статус для ВК^^':
+        elif message.text == 'Статус для ВК^^' or message.text == '/status':
             vk_quote = random.choice(config.vk_quotes)
             bot.send_message(message.chat.id, vk_quote)
         elif message.text == '/help':
-            bot.send_message(message.chat.id, 'Я могу сгенерировать тебе случайный ник, статус или аву, для этого нажми на кнопки внизу (^-^)')
+            bot.send_message(message.chat.id, 'Я могу сгенерировать тебе:\n/nickname\n/status\n/avatar\n(^-^)')
         else:
             bot.send_message(message.chat.id, "Я тебя не понимаю =( Напиши /help")
 
@@ -74,23 +69,4 @@ def callback_inline(call):
      except Exception as e:
          print(repr(e))
 
-# @bot.callback_query_handler(func=lambda call:True)
-# def callback_inline(call):
-#     try:
-#         if call.message:
-#             if call.data == 'good':
-#                 bot.send_message(call.message.chat.id, "Здесь будет веселый статус")
-#             elif call.data == 'bad':
-#                 bot.send_message(call.message.chat.id, "Здесь будет грустный статус")
-#             elif call.data == 'cheeky':
-#                 cheeky_quote = random.choice(config.cheeky_quotes)
-#                 bot.send_message(message.chat.id, cheeky_quote)
-#             elif call.data == 'wise':
-#                 wise_quote = random.choice(config.wise_quotes)
-#                 bot.send_message(call.message.chat.id, wise_quote)
-#
-#     except Exception as e:
-#         print(repr(e))
-# RUN
-
-bot.polling(none_stop=True, interval=0)
+bot.infinity_polling(timeout=10, long_polling_timeout = 5)
